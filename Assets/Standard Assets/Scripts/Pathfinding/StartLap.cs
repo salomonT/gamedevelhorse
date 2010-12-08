29d988644a498c00610adc2385d51eabf4b2ef90
rafter.e <rafter.e@gmail.com>
2010-12-08 22:15:18 +0000

    Update StartLap script AI opponents now able to collide with mandatory object 1/ Final City Level.

--- Assets/Standard Assets/Scripts/Pathfinding/StartLap.cs
+++ Assets/Standard Assets/Scripts/Pathfinding/StartLap.cs
@@ -24,6 +24,29 @@ public class StartLap : MonoBehaviour {
 	GameObject rightWheel;
 	GameObject leftWheel;
 	
+	/**Variables for Snatch & Grab.*/
+	int typeGame;
+	int lapsInRace;
+	bool hasMandatoryOne;
+	bool hasMandatoryTwo;
+	bool hasMandatoryThree;
+	int mandatoryTotal = 0;
+	int mandatoryCount = 0;
+	string mandatory1Name = "";
+	string mandatory2Name = "";
+	string mandatory3Name = "";
+	
+	/**Needed to make Objects Appear Should AI hit them.*/
+	GameObject mandatory2A = GameObject.Find("Mandatories/Mandatory2A");
+	GameObject mandatory2B = GameObject.Find("Mandatories/Mandatory2B");
+	GameObject mandatory2C = GameObject.Find("Mandatories/Mandatory2C");
+	GameObject mandatory2D = GameObject.Find("Mandatories/Mandatory2D");
+	GameObject mandatory3A = GameObject.Find("Mandatories/Mandatory3A");
+	GameObject mandatory3B = GameObject.Find("Mandatories/Mandatory3B");
+	GameObject mandatory3C = GameObject.Find("Mandatories/Mandatory3C");
+	GameObject mandatory3D = GameObject.Find("Mandatories/Mandatory3D");
+	
+	
 	bool monkeyMode = false;
 	
 	
@@ -92,6 +115,32 @@ public class StartLap : MonoBehaviour {
 		currentWaypoint = 0;
 		
 		loadWheel();
+		
+		typeGame = GameManager.getGameType();
+		lapsInRace = GameManager.getLaps();
+		
+		/**Determine How Many Mandatory Items are Needed to Win the Game.*/
+	    if(typeGame == 1)
+	      {
+		    if(lapsInRace == 1)
+		     {
+		       hasMandatoryOne = false;
+			   mandatoryTotal = 1;
+		     }
+		 else if(lapsInRace == 2) 
+	        {
+		 	  hasMandatoryOne = false;
+		      hasMandatoryTwo = false;
+		      mandatoryTotal = 2;
+		   }	
+		 else if(lapsInRace == 3)
+		    {
+		     hasMandatoryOne = false;
+			 hasMandatoryTwo = false;
+			 hasMandatoryThree = false;
+			 mandatoryTotal = 3;
+		   }
+	    }
 		
 		//Choose a radom pickupNextPoint, to make random path.
 		pickNextPoint = (GetComponent (typeof (AIFollow)) as AIFollow).pickNextWaypointDistance;
@@ -251,13 +300,36 @@ public class StartLap : MonoBehaviour {
 	  }
 		
 		if(obj.name == ("FinishLine") && currentWaypoint == 6) {
+	 	  if(typeGame == 1)
+		    {
+			  currentWaypoint = 0;
+			  
+			  if((laps + 1) == mandatoryCount)
+			   {
+				 laps++;  
+			   }
+			   
+			  /**Determine if All Mandatory Items Have Been Collected*/
+		      if(mandatoryCount != mandatoryTotal)
+			    {
+			      /**Determine Which New Set of Mandatory Items Need to Be Displayed*/
+				  if(mandatoryCount == 1)
+				    {
+				      SetMandatoryItemsTwo(mandatory1Name);
+				    }
+				 else if(mandatoryCount == 2)
+				   {
+				     SetMandatoryItemsThree(mandatory2Name);
+				   }
+			    }
+		  }
+		else if(typeGame == 0)			
+		  {
 	 		currentWaypoint = 0;
-	 		//lapTimes[lapCounter] = lapTimeSecondsTotal;
-	 		//lapCounter++;
-	 		//lapTimeMinutes = 0;
-	 		//lapTimeSeconds = 0;
-	 		//lapTimeSecondsTotal = 0;
-			laps++;
+			 laps++;
+		  }	
+		  
+
 		 }	
 		
 		  /**Check For Collisions With Waypoints*/
@@ -294,7 +366,97 @@ public class StartLap : MonoBehaviour {
 		
 		CheckGameManager();
 	}
-	 
+	
+
+	
+	 /**Check for Mandatory Object Collisions.*/
+ public void OnControllerColliderHit(ControllerColliderHit obj)
+{
+  if(obj.collider.name.Contains("Mandatory1"))
+    {
+	  if(hasMandatoryOne == false)
+	    {
+		  mandatory1Name = obj.collider.name;
+		  obj.gameObject.SetActiveRecursively(false);
+          hasMandatoryOne = true;
+		  mandatoryCount = 1;
+		}
+    }	
+	
+  if(obj.collider.name.Contains("Mandatory2"))
+    {
+	  if(hasMandatoryTwo == false && hasMandatoryOne == true)
+	    {
+		  mandatory2Name = obj.collider.name;;		  
+		  obj.gameObject.SetActiveRecursively(false);
+          hasMandatoryTwo = true;
+		  mandatoryCount = 2;
+		}	
+    }	
+	
+  if(obj.collider.name.Contains("Mandatory3"))
+    {
+	  if(hasMandatoryThree == false && hasMandatoryTwo == true)
+	    {
+		  mandatory3Name = obj.collider.name;;		
+		  obj.gameObject.SetActiveRecursively(false);
+          hasMandatoryThree = true;
+		  mandatoryCount = 3;		   
+		}	
+    }	
+}
+	
+ /**
+  *
+  *    SET THE MANDATORY ITEMS TO APPEAR
+  *
+  */
+  
+  /**Mandatory Items Two Appear.*/
+  public void SetMandatoryItemsTwo(string createReplica)
+  {
+    if(createReplica == "Mandatory1A")
+	  {
+	    mandatory2A.SetActiveRecursively(true);
+	  }
+    else if(createReplica == "Mandatory1B")
+	  {
+	   mandatory2B.SetActiveRecursively(true);
+	  }
+    else if(createReplica == "Mandatory1C")
+	  {
+	    mandatory2C.SetActiveRecursively(true);
+	  }
+    else if(createReplica == "Mandatory1D")
+	  {
+	    mandatory2D.SetActiveRecursively(true);
+	  }	  
+  }
+ 
+  /**Mandatory Items Three Appear.*/
+   public void SetMandatoryItemsThree(string createReplica)
+  {
+    if(createReplica == "Mandatory2A")
+	  {
+	    mandatory3A.SetActiveRecursively(true);
+	  }
+    else if(createReplica == "Mandatory2B")
+	  {
+	    mandatory3B.SetActiveRecursively(true);
+	  }
+    else if(createReplica == "Mandatory2C")
+	  {
+	    mandatory3C.SetActiveRecursively(true);
+	  }
+    else if(createReplica == "Mandatory2D")
+	  {
+	    mandatory3D.SetActiveRecursively(true);
+	  }  
+  }
+  
+
+	
+	
 	public void PathComplete (Vector3[] points) {
 		//The points are all the waypoints you need to follow to get to the target
 		//print(gameObject.name);
@@ -345,6 +507,36 @@ public class StartLap : MonoBehaviour {
 
 			}
 		}
+	 else if(GameManager.getGameType() == 1 && GameManager.getLaps() != 0)
+		 {
+		   if(laps >= GameManager.getLaps()) {
+								
+				// Add yourself to the finished race array
+				// Replace THIS HORSE NAME with this horses name
+				GameManager.getFinishedArray().Add("THIS HORSE NAME");
+				
+				//raceCompleted = true;	
+
+				Debug.Log("CPU WON");
+				/*
+				// If there is only 1 player in the array (you) then you won!
+				if(GameManager.getFinishedArray().length == 1) {
+					Debug.Log("You Won!");
+					
+					// Display YOU WON on the screen to this horse
+
+				} else if(GameManager.getFinishedArray().length > 1) {
+					Debug.Log("You Lost!");
+					
+					// Display YOU LOST on the screen
+
+				}*/	 
+		 }
+		
+		// ADD OTHER GAME TYPES HERE, SIMILAR TO ABOVE
+	
+	}
+
 		
 		// ADD OTHER GAME TYPES HERE, SIMILAR TO ABOVE
 	
